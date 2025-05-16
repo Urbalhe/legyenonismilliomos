4f1d13fb24f124f7cdd99a51103a733f606adb69
Urbalhe <szabolcsormos2006@gmail.com>
2025-05-16 13:32:12 +0200

    5. commit :DDDDDDDDD Tesztelve, mukodik :)))))))

--- legyenonismilliomos/Program.cs
+++ legyenonismilliomos/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using legyenonismilliomos;
+
+namespace legyenonismilliomos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Jatek.jatek();
+        }
+    }
+}
