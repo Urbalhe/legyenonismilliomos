99474948732bf617ea716abaa2cbf8ce59786ca1
Urbalhe <szabolcsormos2006@gmail.com>
2025-05-16 12:46:02 +0200

    most vettem eszre hogy eddig maradj talpon-t csinaltam, azt ne nezze, ez a jo :))))

--- legyenonismilliomos/Sorkerdes.cs
+++ legyenonismilliomos/Sorkerdes.cs
@@ -8,5 +8,23 @@ namespace legyenonismilliomos
 {
     internal class Sorkerdes
     {
+        private string kerdesSzoveg;
+        private string[] valaszok = new string[4];
+        private string helyesValaszKod;
+        private string kategoria;
+
+        public Sorkerdes(string kerdesSzoveg, string[] valaszok, string helyesValaszKod, string kategoria)
+        {
+            this.kerdesSzoveg = kerdesSzoveg;
+            this.valaszok = valaszok;
+            this.helyesValaszKod = helyesValaszKod;
+            this.kategoria = kategoria;
+        }
+
+        public string KerdesSzoveg { get => kerdesSzoveg; set => kerdesSzoveg = value; }
+        public string[] Valaszok { get => valaszok; set => valaszok = value; }
+        public string HelyesValaszKod { get => helyesValaszKod; set => helyesValaszKod = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
     }
+
 }
