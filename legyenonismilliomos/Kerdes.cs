9880f17e8b11591d2e6cfa24f1bbe1e06cf0ba23
Urbalhe <szabolcsormos2006@gmail.com>
2025-05-16 12:34:12 +0200

    Add project files.

---
+++ legyenonismilliomos/Kerdes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace legyenonismilliomos
+{
+    public class Kerdes
+    {
+        private string kerdesSzoveg;
+        private string[] valaszok = new string[4];
+        private string helyesValaszKod;
+        private string kategoria;
+
+        public Kerdes(string kerdesSzoveg, string[] valaszok, string helyesValaszKod, string kategoria)
+        {
+            this.kerdesSzoveg = kerdesSzoveg;
+            this.valaszok = valaszok;
+            this.helyesValaszKod = helyesValaszKod;
+            this.kategoria = kategoria;
+        }
+
+        public string KerdesSzoveg { get => kerdesSzoveg; set => kerdesSzoveg = value; }
+        public string[] Valaszok { get => valaszok; set => valaszok = value; }
+        public string HelyesValaszKod { get => helyesValaszKod; set => helyesValaszKod = value; }
+        public string Kategoria { get => kategoria; set => kategoria = value; }
+    }
+}
