5043444b9133977dcad28e30a291bf67dbba7fd2
Urbalhe <szabolcsormos2006@gmail.com>
2025-05-16 13:12:01 +0200

    3. commit, :)))))

--- legyenonismilliomos/Kerdes.cs
+++ legyenonismilliomos/Kerdes.cs
@@ -7,23 +7,28 @@ using System.Threading.Tasks;
 namespace legyenonismilliomos
 {
     public class Kerdes
+
     {
+        private int nehezseg;
         private string kerdesSzoveg;
         private string[] valaszok = new string[4];
-        private string helyesValaszKod;
+        private char helyesValaszKod;
         private string kategoria;
 
-        public Kerdes(string kerdesSzoveg, string[] valaszok, string helyesValaszKod, string kategoria)
+
+        public Kerdes(int nehezseg, string kerdesSzoveg, string[] valaszok, char helyesValaszKod, string kategoria)
         {
+            this.nehezseg = nehezseg;
             this.kerdesSzoveg = kerdesSzoveg;
             this.valaszok = valaszok;
             this.helyesValaszKod = helyesValaszKod;
             this.kategoria = kategoria;
+            
         }
-
+        public int Nehezseg { get => nehezseg; set => nehezseg = value; }
         public string KerdesSzoveg { get => kerdesSzoveg; set => kerdesSzoveg = value; }
         public string[] Valaszok { get => valaszok; set => valaszok = value; }
-        public string HelyesValaszKod { get => helyesValaszKod; set => helyesValaszKod = value; }
+        public char HelyesValaszKod { get => helyesValaszKod; set => helyesValaszKod = value; }
         public string Kategoria { get => kategoria; set => kategoria = value; }
     }
 }
--- legyenonismilliomos/Kerdesek.cs
+++ legyenonismilliomos/Kerdesek.cs
@@ -8,5 +8,35 @@ namespace legyenonismilliomos
 {
     internal class Kerdesek
     {
+        public static List<Sorkerdes> SKBeolvasas()
+        {
+            List<Sorkerdes> Kerdesek = [];
+
+            string[] lines = File.ReadAllLines("sorkerdes.txt");
+
+            foreach (string line in lines)
+            {
+                string[] reszek = line.Split(';');
+                Sorkerdes kerdes = new Sorkerdes(reszek[0], new string[] { reszek[1], reszek[2], reszek[3], reszek[4] }, reszek[5], reszek[6]);
+                Kerdesek.Add(kerdes);
+            }
+            return Kerdesek;
+        }
+        
+        public static List<Kerdes> KBeolvasas()
+        {
+            List<Kerdes> Kerdesek = new List<Kerdes>();
+
+            string[] lines = File.ReadAllLines("kerdes.txt");
+
+            foreach (string line in lines)
+            {
+                string[] reszek = line.Split(';');
+                Kerdes kerdes = new Kerdes(int.Parse(reszek[0]), reszek[1], new string[] { reszek[2], reszek[3], reszek[4], reszek[5] }, char.Parse(reszek[6]), reszek[7]);
+                Kerdesek.Add(kerdes);
+            }
+            return Kerdesek;
+        }
+
     }
 }
